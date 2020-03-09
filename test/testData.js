e46d6ef9027acfd84b6d857ee50dee9ddbe1f08b
Matt Maginniss <matthew.maginniss@gmail.com>
2020-03-08 23:49:43 -0400

    Created tests for roman numeral conversions

--- test/testData.js
+++ test/testData.js
@@ -12,3 +12,24 @@ describe('Data Checks', function () {
         data.getMonthDayName(11, 29).should.equal('Fructidor Panier');
     });
 });
+
+describe('Year Checks', function () {
+    it('First Republican Year', function () {
+        data.getYear(1792).should.equal("I");
+    });
+});
+
+describe('Roman Numeral Conversion', function () {
+    it('1 = Roman Numeral: I', function () {
+        data.convertToRoman(1).should.equal('I');
+    });
+
+    it('469 = Roman Numeral: CDLXIX', function () {
+        data.convertToRoman(469).should.equal('CDLXIX');
+    });
+
+    it('999 = Roman Numeral: CMXCIX', function () {
+        data.convertToRoman(999).should.equal('CMXCIX');
+    });
+});
+
