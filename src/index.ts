9601f14c8249bf16f368b58e3d262bab8babfe09
Matt Maginniss <matthew.maginniss@gmail.com>
2022-06-27 23:11:29 -0400

    Exported info

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 var republicancal = require('./calendar')
 var decimaltime = require('./time')
+var republicanData = require('./data/info')
 
 module.exports.calendar = republicancal 
 
 module.exports.time = decimaltime
+
+module.exports.info = republicanData
